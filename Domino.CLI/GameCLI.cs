f97bd4af86a915d279aefde1b398752ff737c3c4
renadeZ <mzidanenaufalr@gmail.com>
2026-04-17 11:30:04 +0700

    ui

---
+++ Domino.CLI/GameCLI.cs
@@ -0,0 +1,40 @@
+﻿using Domino.Backend;
+using Domino.Backend.Models;
+using Domino.Backend.Models.Enums;
+using Domino.Backend.Models.EventArgs;
+
+public class GameCLI
+{
+    private IGameController _gameController;
+    private const int BOARD_WIDTH = 40;
+    private const int BOARD_HEIGHT = 70;
+    
+    //Colors
+
+
+    public GameCLI(IGameController gameController)
+    {
+        _gameController = gameController ?? throw new ArgumentNullException(nameof(gameController));
+        Console.CursorVisible = false;
+    }
+
+
+    public void GameMain()
+    {
+        try
+        {
+            ConsoleSetup();
+        }
+        catch
+        {
+            
+        }
+    }
+
+    public void ConsoleSetup()
+    {
+        Console.Clear();
+        Console.CursorVisible = false;
+        
+    }
+}
